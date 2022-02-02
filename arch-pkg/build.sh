5647712df7e465c4c6683c8d26a3e422b0abfe12
Henry-ZHR <henry-zhr@qq.com>
2022-02-02 16:49:29 +0800

    Fix

--- arch-pkg/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-pacman -Syu --noconfirm
-pacman -S --noconfirm sudo
-
-useradd --no-create-home --shell /bin/sh build
-echo "build ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-su -c "makepkg -s --noconfirm" build
