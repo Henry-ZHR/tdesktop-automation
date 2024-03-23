d3804d67106df7b57ebc606b6b5bf507cc4deb53
Henry-ZHR <henry-zhr@qq.com>
2024-03-23 15:29:17 +0800

    Fix get-info

--- get-info.py
+++ get-info.py
@@ -4,5 +4,5 @@ import os
 with open(os.getenv('GITHUB_OUTPUT'), 'a') as github_output:
     info = json.load(open('info.json', 'r'))
     for (key, value) in info.items():
-        print(f'${key}: ${value}')
-        print(f'${key}=${value}', file=github_output)
+        print(f'{key}: {value}')
+        print(f'{key}={value}', file=github_output)
