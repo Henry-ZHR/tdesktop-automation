390fff1a6a5c7807fd2b3428866bd61c08ecb366
Henry-ZHR <henry-zhr@qq.com>
2024-03-26 12:42:08 +0800

    Fix notify

--- notify.py
+++ notify.py
@@ -6,6 +6,6 @@ from telegram import Bot
 bot = Bot(os.getenv('TELEGRAM_TOKEN'))
 asyncio.run(
     bot.send_message(
-        getenv('TELEGRAM_TO'),
+        os.getenv('TELEGRAM_TO'),
         f'{os.getenv("VERSION")} build succeeded\nSummary: {os.getenv("SUMMARY_URL")}'
     ))
