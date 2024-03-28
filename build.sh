87aeba8c32217039e3c58b9e3a502105b2ddc936
Henry-ZHR <henry-zhr@qq.com>
2024-03-28 13:38:41 +0800

    Refactor build process & add linker flags

--- build.sh
+++ build.sh
@@ -5,12 +5,16 @@ export SHELLOPTS
 
 readonly PKG_URL=https://gitlab.archlinux.org/archlinux/packaging/packages/telegram-desktop.git
 
-echo "::group::Install dependency"
-pacman --sync --refresh --sysupgrade --needed --noconfirm git
+echo "::group::Install dependencies"
+pacman --sync --sysupgrade --refresh --needed --noconfirm devtools git
+echo "::endgroup::"
+
+echo "::group::Setup makepkg configuration"
+cp --force --verbose /usr/share/devtools/makepkg.conf.d/x86_64.conf /etc/makepkg.conf
 echo "::endgroup::"
 
 echo "::group::Checkout pkg"
-git clone --branch "${VERSION}" --single-branch "${PKG_URL}" pkg
+git clone --branch "${PKGVER}" --single-branch "${PKG_URL}" pkg
 echo "::endgroup::"
 
 echo "::group::Apply pkg patch"
@@ -21,17 +25,32 @@ echo "::group::Apply pkg patch"
 echo "::endgroup::"
 
 echo "::group::Prepare to build"
-useradd --no-create-home --shell /bin/sh build
-echo "build ALL=(ALL:ALL) NOPASSWD: ALL" | tee --append /etc/sudoers >/dev/null
+useradd --no-create-home --shell /bin/sh builduser
+echo "builduser ALL=(ALL:ALL) NOPASSWD: ALL" | tee --append /etc/sudoers >/dev/null
 chmod a+w pkg
 echo "::endgroup::"
 
-echo "::group::Build package"
+echo "::group::Makepkg prepare"
+(
+  cd pkg
+  su --command "makepkg --nobuild --syncdeps --noconfirm" builduser
+)
+echo "::endgroup::"
+
+echo "::group::Apply tdesktop patches"
+(
+  declare -r patches_dir="$(pwd)/patches/tdesktop"
+  cd "pkg/src/tdesktop-${PKGVER}-full"
+  mkdir --parents .git
+  git apply --verbose ${patches_dir}/*.patch
+)
+echo "::endgroup::"
+
+echo "::group::Makepkg build"
 (
   set -x
   cd pkg
   export CMAKE_BUILD_PARALLEL_LEVEL=$(( $(nproc) - 1 ))
-  su --command "makepkg --syncdeps --noconfirm" build
-  set +x
+  su --command "makepkg --noextract" builduser
 )
 echo "::endgroup::"
