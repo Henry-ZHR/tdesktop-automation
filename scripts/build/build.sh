5990b679f549a3673343df10c1e3909c14e1147f
Henry-ZHR <henry-zhr@qq.com>
2024-12-26 22:07:12 +0800

    Makepkg noconfirm

--- scripts/build/build.sh
+++ scripts/build/build.sh
@@ -5,7 +5,7 @@ for patch in ../patches/pkg/*.patch
 do
   patch --strip=1 --input="${patch}"
 done
-makepkg --nobuild --syncdeps
+makepkg --nobuild --syncdeps --noconfirm
 for patch in ../patches/tdesktop/*.patch
 do
   patch --strip=1 --input="${patch}"
