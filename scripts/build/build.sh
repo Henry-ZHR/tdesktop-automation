230da852c9b0d3847bfd58dcf4907462bc156b1b
Henry-ZHR <henry-zhr@qq.com>
2024-12-28 15:20:41 +0800

    Group outputs of build.sh

--- scripts/build/build.sh
+++ scripts/build/build.sh
@@ -1,16 +1,39 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
-cd telegram-desktop
-for patch in ../patches/pkg/*.patch
-do
-  patch --strip=1 --input="${patch}"
-done
-makepkg --nobuild --syncdeps --noconfirm
-(
-  cd src/tdesktop-*-full
-  for patch in ../../../patches/tdesktop/*.patch
+
+function start_group() {
+  echo "::group::$1"
+}
+function end_group() {
+  echo "::endgroup::"
+}
+
+function apply_patches() {
+  for patch in "$@"
   do
+    echo "Applying patch: ${patch}"
     patch --strip=1 --input="${patch}"
   done
+}
+
+
+cd telegram-desktop
+
+start_group "Patch PKGBUILD"
+apply_patches ../patches/pkg/*.patch
+end_group
+
+start_group "makepkg prepare"
+makepkg --nobuild --syncdeps --noconfirm
+end_group
+
+(
+  start_group "Patch src"
+  cd src/tdesktop-*-full
+  apply_patches ../../../patches/tdesktop/*.patch
+  end_group
 )
+
+start_group "makepkg build & package"
 makepkg --noextract
+end_group
